6a4a050e138169ab726c69639f7f3f59aa695af4
danielevergani <dvergani94@gmail.com>
2021-03-15 15:32:58 +0100

    modificato comportamento al click
    aggiunge/rimuove classe active

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $("a").click (function(){
-    $(this).next().slideToggle();
+
+    if ( $(this).next().hasClass("dropdown_active") ){
+        $(".dropdown").removeClass("dropdown_active");
+        $(this).next().removeClass("dropdown_active")
+    }
+    else{
+        $(".dropdown").removeClass("dropdown_active");
+        $(this).next().addClass("dropdown_active");
+    }
 });
+
+// $("a").mouseenter (function(){
+//     $(this).next().show();
+// });
+
+// $("a").mouseleave (function(){
+//     $(this).next().hide();
+// });
+
+
